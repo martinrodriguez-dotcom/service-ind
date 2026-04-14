3803e17db03dd5603314fa374488f7a30a1742e3
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-04-14 20:21:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Montaje principal de la aplicación React
+// Montaje principal de la aplicación
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
