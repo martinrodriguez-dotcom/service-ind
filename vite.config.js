c50326eb13fa5815a4ea5844986f4c1705ba1421
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-04-14 20:11:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Esto asegura que las rutas funcionen en cualquier servidor (Netlify/GitHub)
-  base: './', 
-  build: {
-    outDir: 'dist',
-  }
 })
