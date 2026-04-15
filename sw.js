dfaba060ca4a04485cfcc57523ac4825969a3316
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-04-15 13:19:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Aumentamos la versión para forzar la actualización en los celulares
-const CACHE_NAME = 'bnd-v2.3';
+const CACHE_NAME = 'bnd-v2.4';
 
 // Sumamos style.css a la lista de archivos offline
 const ASSETS_TO_CACHE = [
